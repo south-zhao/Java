c35328cb084562f3127a3ce23d79a84d60d6d76f
south-zhao <2252585312@qq.com>
2022-11-24 19:27:41 +0800

    java

--- src/unit6/Solution6_4.java
+++ src/unit6/Solution6_4.java
@@ -9,29 +9,53 @@ package unit6;
  * @Describe: -*- coding: gbk -*-
  */
 public class Solution6_4 {
-    public static void main(String[] agrs) {
-        String[] e = {"for", "tea", "too"};
-        String f = (e.length > 0) ? e[0]:null;
-        System.out.println(f);
+    public static void main(String[] args) {
+        Simulator simulator = new Simulator();
+        simulator.playSound(new Cat());
+        simulator.playSound(new Dog());
     }
 }
 
 
 interface Animal {
-    public String cry();
+    public void cry();
     public String getAnimalName();
 }
 
 
-//class Simulator implements Animal {
-//    public String playSound(Animal animal) {
-//
-//    }
-//}
+class Simulator {
+    public void playSound(Animal animal) {
+        animal.cry();
+        String name = animal.getAnimalName();
+        System.out.println("动物的名字是" + name);
+    }
+}
 
 
+class Cat implements Animal {
+    @Override
+    public void cry() {
+        System.out.println("喵喵喵~~");
+    }
+
+    @Override
+    public String getAnimalName() {
+        return "猫";
+    }
+}
 
 
+class Dog implements Animal {
+    @Override
+    public void cry() {
+        System.out.println("汪汪汪~~~");
+    }
+
+    @Override
+    public String getAnimalName() {
+        return "狗";
+    }
+}
 
 
 
